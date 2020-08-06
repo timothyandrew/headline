20b1f26d2f91a93af8b51ab9381a0e7a1e165beb
Timothy Andrew <mail@timothyandrew.net>
2020-08-06 14:54:57 +0530

    get rid of some autogenerated cruft

--- lib/headline_web/controllers/feed_controller.ex
+++ lib/headline_web/controllers/feed_controller.ex
@@ -11,34 +11,4 @@ defmodule HeadlineWeb.FeedController do
     feeds_by_group = RSS.list_feeds_by_group()
     render(conn, "index.json", feeds: feeds, feeds_by_group: feeds_by_group)
   end
-
-  def create(conn, %{"feed" => feed_params}) do
-    with {:ok, %Feed{} = feed} <- RSS.create_feed(feed_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.feed_path(conn, :show, feed))
-      |> render("show.json", feed: feed)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    feed = RSS.get_feed!(id)
-    render(conn, "show.json", feed: feed)
-  end
-
-  def update(conn, %{"id" => id, "feed" => feed_params}) do
-    feed = RSS.get_feed!(id)
-
-    with {:ok, %Feed{} = feed} <- RSS.update_feed(feed, feed_params) do
-      render(conn, "show.json", feed: feed)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    feed = RSS.get_feed!(id)
-
-    with {:ok, %Feed{}} <- RSS.delete_feed(feed) do
-      send_resp(conn, :no_content, "")
-    end
-  end
 end
--- lib/headline_web/controllers/group_controller.ex
+++ lib/headline_web/controllers/group_controller.ex
@@ -11,34 +11,4 @@ defmodule HeadlineWeb.GroupController do
     feeds_by_group = RSS.list_feeds_by_group()
     render(conn, "index.json", groups: groups, feeds_by_group: feeds_by_group)
   end
-
-  def create(conn, %{"group" => group_params}) do
-    with {:ok, %Group{} = group} <- RSS.create_group(group_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.group_path(conn, :show, group))
-      |> render("show.json", group: group)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    group = RSS.get_group!(id)
-    render(conn, "show.json", group: group)
-  end
-
-  def update(conn, %{"id" => id, "group" => group_params}) do
-    group = RSS.get_group!(id)
-
-    with {:ok, %Group{} = group} <- RSS.update_group(group, group_params) do
-      render(conn, "show.json", group: group)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    group = RSS.get_group!(id)
-
-    with {:ok, %Group{}} <- RSS.delete_group(group) do
-      send_resp(conn, :no_content, "")
-    end
-  end
 end
