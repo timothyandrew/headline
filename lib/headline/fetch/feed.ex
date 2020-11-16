4c682416a1777dea5797a6aa0d64636832c02770
Timothy Andrew <mail@timothyandrew.net>
2020-11-16 15:31:09 +0530

    gunzip rss responses

--- lib/headline/fetch/feed.ex
+++ lib/headline/fetch/feed.ex
@@ -19,7 +19,9 @@ defmodule Headline.Fetch.Feed do
 
 
   def fetch(feed) do
-    with {:ok, %{body: body}} <- HTTPoison.get(feed.url, [], follow_redirect: true) do
+    with {:ok, response} <- HTTPoison.get(feed.url, [], follow_redirect: true) do
+      body = maybe_gunzip(response)
+
       atom_entries = xpath(body, ~x"//feed/entry"l, title: ~x"./title/text()"s, url: ~x"./id/text()"s, html: ~x"./content/text()"s)
       rss_entries = xpath(body, ~x"//item"l, title: ~x"./title/text()"s, url: ~x"./link/text()"s, html: ~x"./description/text()"s)
 
@@ -27,6 +29,22 @@ defmodule Headline.Fetch.Feed do
 
       for entry <- entries, include?(entry), do: RSS.create_item(Map.merge(%{feed_id: feed.id}, entry))
       RSS.update_feed(feed, %{last_updated_on_time: Timex.now()})
+    end
+  end
+
+  def maybe_gunzip(response) do
+    gzipped = Enum.any?(response.headers, fn (kv) ->
+      case kv do
+        {"Content-Encoding", "gzip"} -> true
+        {"Content-Encoding", "x-gzip"} -> true
+        _ -> false
+      end
+    end)
+
+    if gzipped do
+      :zlib.gunzip(response.body)
+    else
+      response.body
     end
   end
 
